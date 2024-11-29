cac7b5354bf7b4a64e8958783ffaa514d44a7e7f
Rubén Montero <ruben.montero.vazquez@gmail.com>
2024-11-29 09:48:28 +0100

    Added text area to create post form and stylized the dialog a bit more

--- code/frontend/src/jsx/components/posts/CreatePostTabForm.js
+++ code/frontend/src/jsx/components/posts/CreatePostTabForm.js
@@ -42,40 +42,39 @@ const CreatePostTabForm = (props) => {
 
     return <div className="createPostFormContainer">
         <form onSubmit={onSubmitCreatePost}>
-                <div className="formInput"> { /* FIX-ME: Label animation doesn't collapse (since field it's not required, apparently) */}
-                    <input type="text" value={formTitle}
-                        onChange={e => { setFormTitle(e.target.value) }}
-                        onFocus={e => { e.target.placeholder = "Autores del siglo XVII"; }}
-                        onBlur={e => { e.target.placeholder = ""; }} />
-                    <div className="underline"></div>
-                    <label htmlFor="">Título</label>
-                </div>
+            <div className="postFormFirstRowInputsContainer">
                 <div className="formInputSelect createPostSelect">
                     <select name="unit"
                         value={formUnitId}
                         onChange={e => { setFormUnitId(e.target.value); }} >
-                            <option value={UNIT_UNASSIGNED}>Sin tema</option>
-                            {props.units.map(g => {
-                                return <option value={g.id}>{g.name}</option>
-                            })}
+                        <option value={UNIT_UNASSIGNED}>Sin tema</option>
+                        {props.units.map(g => {
+                            return <option value={g.id}>{g.name}</option>
+                        })}
                     </select>
                 </div>
-                { /* TODO: Make this a TextArea that allows some text styling */ }
-                <div className="formInput">
-                    <input type="text" value={formContent}
-                        onChange={e => { setFormContent(e.target.value) }}
-                        onFocus={e => { e.target.placeholder = "Lost autores del siglo XVII eran..."; }}
-                        onBlur={e => { e.target.placeholder = ""; }}
-                        required />
+                <div className="formInput"> { /* FIX-ME: Label animation doesn't collapse (since field it's not required, apparently) */}
+                    <input className="formInputCreatePostTitle" type="text" value={formTitle}
+                        onChange={e => { setFormTitle(e.target.value) }}
+                        onFocus={e => { e.target.placeholder = "Autores del siglo XVII"; }}
+                        onBlur={e => { e.target.placeholder = ""; }} required />
                     <div className="underline"></div>
-                    <label htmlFor="">Contenido</label>
+                    <label htmlFor="">Título</label>
                 </div>
-                <div className="formSubmit">
-                    <input type="submit" value="Publicar" />
-                </div>
-                {isLoading && <div className="dialogHUDCentered"><LoadingHUD /></div>}
-            </form>
-        </div>
+            </div>
+            <div className="formTextArea">
+                <textarea placeholder="¡Acordaos de tomaros las cosas con calma! Estamos aquí para aprender :)"
+                    value={formContent}
+                    onChange={e => { setFormContent(e.target.value) }}
+                    onFocus={e => { e.target.placeholder = "Los autores del siglo XVII eran..."; }}
+                    required />
+            </div>
+            <div className="formSubmit">
+                <input type="submit" value="Publicar" />
+            </div>
+            {isLoading && <div className="dialogHUDCentered"><LoadingHUD /></div>}
+        </form>
+    </div>
 }
 
 export default CreatePostTabForm;
